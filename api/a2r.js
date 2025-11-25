d8dd7a5cc89e40cf51864ab08ee8c1aa1c774a16
Noel.Ch <noelchimbo@gmail.com>
2025-11-25 08:21:35 -0300

    cambio

--- api/a2r.js
+++ api/a2r.js
@@ -1,30 +1,39 @@
 export default function handler(req, res) {
-    try {
-        const num = parseInt(req.query.arabic);
+    const { arabic } = req.query;
 
-        if (isNaN(num) || num <= 0 || num > 3999)
-            throw new Error("Debe ser un número entre 1 y 3999");
-
-        const table = [
-            { v: 1000, r: "M" }, { v: 900, r: "CM" },
-            { v: 500, r: "D" }, { v: 400, r: "CD" },
-            { v: 100, r: "C" }, { v: 90, r: "XC" },
-            { v: 50, r: "L" }, { v: 40, r: "XL" },
-            { v: 10, r: "X" }, { v: 9, r: "IX" },
-            { v: 5, r: "V" }, { v: 4, r: "IV" },
-            { v: 1, r: "I" }
-        ];
-
-        let n = num, roman = "";
-        for (const t of table) {
-            while (n >= t.v) {
-                roman += t.r;
-                n -= t.v;
-            }
-        }
-
-        res.status(200).json({ arabic: num, roman });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+    if (!arabic || isNaN(arabic)) {
+        return res.status(400).json({
+            error: "Parámetro 'arabic' inválido"
+        });
     }
+
+    const num = parseInt(arabic);
+
+    if (num <= 0 || num > 3999) {
+        return res.status(400).json({
+            error: "El número debe estar entre 1 y 3999"
+        });
+    }
+
+    const tabla = [
+        { v: 1000, r: "M" }, { v: 900, r: "CM" },
+        { v: 500, r: "D" }, { v: 400, r: "CD" },
+        { v: 100, r: "C" }, { v: 90, r: "XC" },
+        { v: 50, r: "L" }, { v: 40, r: "XL" },
+        { v: 10, r: "X" }, { v: 9, r: "IX" },
+        { v: 5, r: "V" }, { v: 4, r: "IV" },
+        { v: 1, r: "I" }
+    ];
+
+    let n = num;
+    let resRoman = "";
+
+    for (const e of tabla) {
+        while (n >= e.v) {
+            resRoman += e.r;
+            n -= e.v;
+        }
+    }
+
+    res.status(200).json({ roman: resRoman });
 }
